2a220abf822b63d5b35695686289a364c9402545
Joshua Beckwith <supahsoftware@gmail.com>
2019-02-23 15:21:54 -0600

    Do the stuff. JB

---
+++ app/src/main/java/supahsoftware/androidexamplesearchbar/SearchView.kt
@@ -0,0 +1,56 @@
+package supahsoftware.androidexamplesearchbar
+
+import android.animation.Animator
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewAnimationUtils
+import android.widget.FrameLayout
+import kotlinx.android.synthetic.main.view_search.view.*
+
+class SearchView(context: Context, attrs: AttributeSet) : FrameLayout(context, attrs) {
+
+    init {
+        LayoutInflater.from(context).inflate(R.layout.view_search, this, true)
+
+        open_search_button.setOnClickListener { openSearch() }
+        close_search_button.setOnClickListener { closeSearch() }
+    }
+
+    private fun openSearch() {
+        search_input_text.setText("")
+        search_open_view.visibility = View.VISIBLE
+        val circularReveal = ViewAnimationUtils.createCircularReveal(
+            search_open_view,
+            (open_search_button.right + open_search_button.left) / 2,
+            (open_search_button.top + open_search_button.bottom) / 2,
+            0f, width.toFloat()
+        )
+        circularReveal.duration = 300
+        circularReveal.start()
+    }
+
+    private fun closeSearch() {
+        val circularConceal = ViewAnimationUtils.createCircularReveal(
+            search_open_view,
+            (open_search_button.right + open_search_button.left) / 2,
+            (open_search_button.top + open_search_button.bottom) / 2,
+            width.toFloat(), 0f
+        )
+
+        circularConceal.duration = 300
+        circularConceal.start()
+        circularConceal.addListener(object : Animator.AnimatorListener {
+            override fun onAnimationRepeat(animation: Animator?) = Unit
+            override fun onAnimationCancel(animation: Animator?) = Unit
+            override fun onAnimationStart(animation: Animator?) = Unit
+            override fun onAnimationEnd(animation: Animator?) {
+                search_open_view.visibility = View.INVISIBLE
+                search_input_text.setText("")
+                circularConceal.removeAllListeners()
+            }
+        })
+    }
+
+}
